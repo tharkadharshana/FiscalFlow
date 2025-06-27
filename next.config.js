577490095eb8d9c7b865e37f204ba32cd6aa10ea
Tharka Karunanayake <tharkadharshana@gmail.com>
2025-06-27 13:22:00 +0000

    i directly went to this "https://coin-images.coingecko.com/coins/images/

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
